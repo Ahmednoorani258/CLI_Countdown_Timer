fded2e7def117030fd805662d358da9f34f6e2c8
Muhammad Ahmed Noorani <ahmednoorani258@gmail.com>
2024-05-23 09:18:48 -0700

    update

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+async function timer (){
+  let input = await inquirer.prompt({
+    name:"timer",
+    type:"number",
+    message: "Pls Enter The Count Down Time in Seconds{1-60}",
+    validate:(value => {
+      if(value < 1 || value > 60){
+        return "Please Enter a Number Between 1 and 60";
+      }else{
+      return true;
+      }})
+  })
+  
+  let count =0
+  let starttime = setInterval(function(){
+    count++
+    let cond = count <= 9? `00.0${count}`:`00.${count}`;
+    console.log(cond);
+    if(count === input.timer ){
+      clearInterval(starttime)
+    }
+  },1000)
+}
+  
+
+timer()
